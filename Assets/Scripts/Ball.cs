3dc8df5eebf97f3c2847b5c23f59dabc02e0cc43
Nir Boneh <nir.boneh@geomag.info>
2016-05-11 01:35:00 -0600

    Adding particle effect on collision

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -6,10 +6,11 @@ public class Ball : MonoBehaviour {
     float stretchFactor;
     float radius;
     public AudioClip bouncesound;
-    
+    public ParticleSystem particleCollision;
 
-    private GameObject prevColidedGameObject;
-    private float timeTillColideAgain = 0.0f;
+
+    private GameObject prevCollidedGameObject;
+    private float timeUntilCollideAgain = 0.0f;
 
     private AudioSource source;
     // Use this for initialization
@@ -52,17 +53,15 @@ public class Ball : MonoBehaviour {
         Vector3 forward = rb.transform.forward;
         Vector3 vel = rb.velocity;
 
-        float velx = (Vector3.Dot(right, vel)/60.0f) * stretchFactor;
-        float vely = (Vector3.Dot(up, vel) / 60.0f) * stretchFactor;
-        float velz =( Vector3.Dot(forward, vel) / 60.0f) * stretchFactor;
+        float velx = (Vector3.Dot(right, vel)/40.0f) * stretchFactor;
+        float vely = (Vector3.Dot(up, vel) / 40.0f) * stretchFactor;
+        float velz =( Vector3.Dot(forward, vel) / 40.0f) * stretchFactor;
 
         float stretchX = radius - velx + vely / 2.0f + velz / 2.0f;
         float stretchY = radius + velx / 2.0f - vely + velz / 2.0f;
         float stretchZ = radius + velx / 2.0f + vely / 2.0f - velz;
 
-        rb.transform.localScale = new Vector3(stretchX,
-            stretchY,
-            stretchZ);
+        rb.transform.localScale = new Vector3(stretchX,stretchY,stretchZ);
 
         if (stretchFactor > 0.0f)
         {
@@ -73,36 +72,45 @@ public class Ball : MonoBehaviour {
             }
         }
 
-        if(timeTillColideAgain > 0.0f)
+        if(timeUntilCollideAgain > 0.0f)
         {
-            timeTillColideAgain -= t * 10.0f;
-            if (timeTillColideAgain < 0.0f)
+            timeUntilCollideAgain -= t * 10.0f;
+            if (timeUntilCollideAgain < 0.0f)
             {
-                timeTillColideAgain = 0.0f;
+                timeUntilCollideAgain = 0.0f;
             }
         }
+
     }
 
 
     void OnCollisionEnter(Collision collision)
     {
-        //ContactPoint contact = collision.contacts[0];
-        //Quaternion rot = Quaternion.FromToRotation(Vector3.up, contact.normal);
-       // Vector3 pos = contact.point;
-        //Instantiate(explosionPrefab, pos, rot);
+
 
         if (collision.gameObject.name == "Plane")
             return;
 
-        if (timeTillColideAgain > 0.0f && prevColidedGameObject == collision.gameObject)
+        if (timeUntilCollideAgain > 0.0f && prevCollidedGameObject == collision.gameObject)
             return;
 
 
+        ContactPoint contact = collision.contacts[0];
+        Quaternion rot = Quaternion.FromToRotation(Vector3.forward, contact.normal);
+        Vector3 pos = contact.point;
+        ParticleSystem particleEffect = (ParticleSystem)Instantiate(particleCollision, pos, rot);
+        particleEffect.startSize = collision.relativeVelocity.magnitude/10.0f;
+        particleEffect.startSpeed = collision.relativeVelocity.magnitude / 10.0f;
+        ParticleSystem.ShapeModule shape = particleEffect.shape;
+        shape.radius = radius / 4.0f;
 
-        timeTillColideAgain = 0.5f;
-        prevColidedGameObject = collision.gameObject;
-        source.PlayOneShot(bouncesound, (collision.relativeVelocity.magnitude * collision.relativeVelocity.magnitude) / 10000.0f);
-        stretchFactor = collision.relativeVelocity.magnitude/3.0f;
+
+        timeUntilCollideAgain = 0.5f;
+        prevCollidedGameObject = collision.gameObject;
+        source.PlayOneShot(bouncesound, (collision.relativeVelocity.magnitude * collision.relativeVelocity.magnitude) / 1000.0f);
+        stretchFactor = 4.0f;
+
+        Destroy(particleEffect, particleCollision.duration);
 
     }
 }
---
+++ Assets/Scripts/FieldView.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FieldView : MonoBehaviour {
+
+    public float width = 16f;
+    public float height = 9f;
+
+
+    void Start()
+    {
+        SetResoluton();
+    }
+
+    void SetResoluton()
+    {
+        GetComponent<Camera>().aspect = width / height;
+    }
+}
